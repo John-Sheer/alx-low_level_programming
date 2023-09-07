4c1ede1f7171eef53a365afc6a95521139c5c665
john-sheer <meskojohn@gmail.com>
2023-09-07 16:51:05 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - prints "with proper grammar, but the outcome is a piece of art," followed newline
- * return/ Always O.
+ * Return: Always O.
  */
 int main(void)
 {
