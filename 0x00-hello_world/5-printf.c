edda3a7385c03875e40947767c372730c8d03d9b
john-sheer <meskojohn@gmail.com>
2023-09-07 16:46:29 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,8 @@
 /**
  * main - prints "with proper grammar, but the outcome is a piece of art," followed newline
  * return/ Always O.
- *
  */
-
 int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n";
 	return (0);
