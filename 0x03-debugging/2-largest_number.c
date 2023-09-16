037c6d961bb9090aee9ba3dc05e71b5de268e380
john-sheer <meskojohn@gmail.com>
2023-09-15 18:25:53 -0700

    task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,7 +9,8 @@
  */
 
 int largest_number(int a, int b, int c)
-	{
+
+{
 	int largest;
 
 	if (a > b && a > c)
@@ -26,4 +27,4 @@ int largest_number(int a, int b, int c)
 	}
 
 	return (largest);
-	}
+}
