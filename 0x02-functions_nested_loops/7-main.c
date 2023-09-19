02e832b04cc735147ff810875dfd89b6b675a38b
john-sheer <meskojohn@gmail.com>
2023-09-19 15:13:16 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -5,14 +5,20 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int print_last_digit(int n)
 {
-    int r;
+	int a;
 
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
+	if (n < 0)
+	n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
+
