950d2ba7662569b49654ecfe0dc955cda9e095d6
john-sheer <meskojohn@gmail.com>
2023-09-30 14:14:47 -0700

    0x08-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
-/**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0
- */
+//
+// * _puts_recursion - function like puts();
+// * @s: input
+// * Return: Always 0
+
 void _puts_recursion(char *s)
 {
 	if (*s)
