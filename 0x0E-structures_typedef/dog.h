3eb89a3cb6e78313167764d2528934d996b022fd
john-sheer <meskojohn@gmail.com>
2023-10-10 16:02:04 -0700

    structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,21 +2,6 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's info
- * @name: member one
- * @age: member two
- * @owner: member three
- *
- * Description: Longer description
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
-/**
  * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
