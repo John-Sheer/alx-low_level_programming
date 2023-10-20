88d6750357045217f5afe8fbb2880df371baa50e
John-Sheer <meskojohn@gmail.com>
2023-10-19 17:55:27 -0700

    task3

--- 0x12-singly_linked_lists/3-main.c
+++ 0x12-singly_linked_lists/3-main.c
@@ -9,38 +9,44 @@
  * Return: Always 0.
  */
 int main(void)
+list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *head;
 
-    head = NULL;
-    add_node_end(&head, "Anne");
-    add_node_end(&head, "Colton");
-    add_node_end(&head, "Corbin");
-    add_node_end(&head, "Daniel");
-    add_node_end(&head, "Danton");
-    add_node_end(&head, "David");
-    add_node_end(&head, "Gary");
-    add_node_end(&head, "Holden");
-    add_node_end(&head, "Ian");
-    add_node_end(&head, "Ian");
-    add_node_end(&head, "Jay");
-    add_node_end(&head, "Jennie");
-    add_node_end(&head, "Jimmy");
-    add_node_end(&head, "Justin");
-    add_node_end(&head, "Kalson");
-    add_node_end(&head, "Kina");
-    add_node_end(&head, "Matthew");
-    add_node_end(&head, "Max");
-    add_node_end(&head, "Michael");
-    add_node_end(&head, "Ntuj");
-    add_node_end(&head, "Philip");
-    add_node_end(&head, "Richard");
-    add_node_end(&head, "Samantha");
-    add_node_end(&head, "Stuart");
-    add_node_end(&head, "Swati");
-    add_node_end(&head, "Timothy");
-    add_node_end(&head, "Victor");
-    add_node_end(&head, "Walton");
-    print_list(head);
-    return (0);
+	unsigned int i;
+	list_t *new;
+	list_t *temp;
+
+	new  = malloc(sizeof(list_t));
+
+	if (str == NULL)
+		return (NULL);
+
+	if (new == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		new->len = i;
+		new->str = strdup(str);
+		new->next = NULL;
+		return (new);
+	}
+
+	temp = *head;
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new;
+
+	new->len = i;
+	new->str = strdup(str);
+	new->next = NULL;
+
+	return (new);
+
 }
